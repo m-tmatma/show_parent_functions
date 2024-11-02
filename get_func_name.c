6dda287ff385baae8ed357c417734a87b641361c
Masaru Tsuchiyama <m.tmatma@gmail.com>
2024-11-03 08:56:21 +0900

    親の親を追加

--- get_func_name.c
+++ get_func_name.c
@@ -1,25 +1,34 @@
+/*
+    gcc get_func_name.c  -ldl -rdynamic
+*/
 #include <stdio.h>
 #define __USE_GNU
 #include <dlfcn.h>
 
-void hoge() {
-    Dl_info info;
-    dladdr(__builtin_return_address(0), &info);
-    printf("[%s] parent func name => %p [%s]\n",
+void hoge3() {
+    Dl_info info[3];
+
+    dladdr(__builtin_return_address(0), &info[0]);
+    dladdr(__builtin_return_address(1), &info[1]);
+    dladdr(__builtin_return_address(2), &info[2]);
+    printf("[%s] parent func name => %p[%s] => %p[%s] => %p[%s]\n",
            __func__,
-           __builtin_return_address(0),
-           info.dli_sname);
+           __builtin_return_address(2), info[2].dli_sname,
+           __builtin_return_address(1), info[1].dli_sname,
+           __builtin_return_address(0), info[0].dli_sname
+    );
+}
+
+void hoge2() {
+    hoge3();
+}
+
+void hoge() {
+    hoge2();
 }
 
 void foo() {
     hoge();
-
-    Dl_info info;
-    dladdr(__builtin_return_address(0), &info);
-    printf("[%s] parent func name => %p [%s]\n",
-           __func__,
-           __builtin_return_address(0),
-           info.dli_sname);
 }
 
 int main(int argc, char const* argv[])
